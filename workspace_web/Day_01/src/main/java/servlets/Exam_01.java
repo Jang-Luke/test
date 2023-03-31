7f03d6410d040d9ecf390b9db5cd91206c6dc607
Jang-Luke <lujke555@gmail.com>
2023-03-31 16:26:23 +0900

    230331 / 중간 저장, servlet 예제

--- workspace_web/Day_01/src/main/java/servlets/Exam_01.java
+++ workspace_web/Day_01/src/main/java/servlets/Exam_01.java
@@ -1,6 +1,8 @@
 package servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,10 +14,14 @@ import javax.servlet.http.HttpServletResponse;
 public class Exam_01 extends HttpServlet {
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String msg = request.getQueryString().split("=")[1];
+		String msgPara = request.getParameter("msg");
+		PrintWriter resp = response.getWriter();
+		resp.append(msgPara + " Complete!");
+		System.out.println("여기가 Get 입니다. 이 출력이 처리하는 코드라고 가정합니다. "+msg);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		System.out.println("여기가 Post 입니다.");
 	}
 }
