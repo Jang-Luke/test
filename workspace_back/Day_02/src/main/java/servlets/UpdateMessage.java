bfc907607c33d71e4cf5b21f69c33f85116f037c
Jang-Luke <lujke555@gmail.com>
2023-04-04 12:14:22 +0900

    230404 / 오류 해결(오타)

--- workspace_back/Day_02/src/main/java/servlets/UpdateMessage.java
+++ workspace_back/Day_02/src/main/java/servlets/UpdateMessage.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("UpdateMessage")
+@WebServlet("/UpdateMessage")
 public class UpdateMessage extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
