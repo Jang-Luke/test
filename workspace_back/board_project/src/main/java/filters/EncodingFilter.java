6c5fb35db61c453d901bdc9af86d4c38c113883f
Jang-Luke <lujke555@gmail.com>
2023-04-07 14:17:03 +0900

    1

--- workspace_back/board_project/src/main/java/filters/EncodingFilter.java
+++ workspace_back/board_project/src/main/java/filters/EncodingFilter.java
@@ -2,8 +2,10 @@ package filters;
 
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
 
+@WebFilter("/*")
 public class EncodingFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
