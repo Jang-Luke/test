389934b0d9e41abae9468b0e1f2301f4784fc55f
Jang-Luke <lujke555@gmail.com>
2023-05-29 14:47:34 +0900

    dd
    dd

---
+++ spring/websocket4/src/main/java/kh/spring/websocket4/Websocket4Application.java
@@ -0,0 +1,13 @@
+package kh.spring.websocket4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Websocket4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Websocket4Application.class, args);
+    }
+
+}
---
+++ spring/websocket4/src/test/java/kh/spring/websocket4/Websocket4ApplicationTests.java
@@ -0,0 +1,13 @@
+package kh.spring.websocket4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Websocket4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
